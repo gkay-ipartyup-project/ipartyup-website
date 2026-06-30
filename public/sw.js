d17a13f359e73937eb11787511885ab0baaf41c0
Gkay7hells <ayaanihanni@gmail.com>
2026-06-30 17:18:02 +0500

    feat: add monetag and adsterra ad tags, implement lucide icons, fix brolink font path, and add theme toggle

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11109151
+    "domain": "3nbf4.com",
+    "zoneId": 11220356
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
